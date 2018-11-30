730ad1af354c3ca0e34f3fd3136eaf81282aaf6e
qishichang <qishichang@live.com>
2018-12-01 11:12:11 +1300

    add RemoteIpMessageProvider

---
+++ MultiTenantApp/Features/RemoteIp/RemoteIpMessageProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using MultiTenantApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiTenantApp.Features.RemoteIp
+{
+    public class RemoteIpMessageProvider : IMessageProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public RemoteIpMessageProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Task<string> GetMessageAsync()
+        {
+            var remoteIpAddress = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress;
+            return Task.FromResult($"Your IP Address: {remoteIpAddress}");
+        }
+    }
+}
---
+++ MultiTenantApp/Features/RemoteIp/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MultiTenantApp.Services;
+using OrchardCore.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+[assembly: OrchardCore.Modules.Manifest.Feature(
+    Id = "RemoteIp",
+    Name = "RemoteIp"
+)]
+
+namespace MultiTenantApp.Features.RemoteIp
+{
+    [Feature("RemoteIp")]
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IMessageProvider, RemoteIpMessageProvider>();
+        }
+    }
+}
