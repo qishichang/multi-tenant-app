0e93527e784a1b78515659f40d5ba60fea4c2deb
qishichang <qishichang@live.com>
2018-12-01 10:13:45 +1300

    add multi-tenant settings

--- MultiTenantApp/Startup.cs
+++ MultiTenantApp/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Environment.Shell;
 
 namespace MultiTenantApp
 {
@@ -15,6 +16,8 @@ namespace MultiTenantApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOrchardCore()
+                .WithTenants();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +28,17 @@ namespace MultiTenantApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseOrchardCore(a => a.Run(async context =>
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                // ShellSettings provide the tenant's configuration.
+                var shellSettings = context.RequestServices.GetRequiredService<ShellSettings>();
+
+                // Read the tenant-specific custom settings.
+                var customSetting = shellSettings.Configuration["CustomSetting"];
+
+                // Write the value of the custom setting to the response stream.
+                await context.Response.WriteAsync(customSetting);
+            }));
         }
     }
 }
